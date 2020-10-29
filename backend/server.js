eb9b329e33f3783686aa9c8f28f8d1913024c2df
Olaoluwa IBUKUN <ibukundaniel402@yahoo.co.uk>
2020-10-29 10:21:34 +0000

    testing v1

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,6 @@ app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/challenges", challengeRoutes);
 
-const __dirname = path.resolve();
-
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/build")));
 
