8d165123858bfd3e04a6d6b320d313b4cdc800f4
Olaoluwa IBUKUN <ibukundaniel402@yahoo.co.uk>
2020-10-29 11:28:18 +0000

    jhghg

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,6 +1,6 @@
-const asyncHandler = require("express-async-handler");
-const User = require("../models/userModel.js");
-const generateToken = require("../utils/generateToken.js");
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
 // @desc Auth user, get token
 // @route POST /api/users/login
